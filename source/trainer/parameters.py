4fd7dc252433b665831df8582746e28bcb0218d2
janezlapajne <lapajne.janez@gmail.com>
2024-10-07 12:11:14 +0200

    Refactor: parameters separation

--- source/trainer/parameters.py
+++ source/trainer/parameters.py
@@ -1,23 +1,37 @@
 from siapy.optimizers.parameters import ParametersDictType
 
-savgol_xgb: ParametersDictType = {
-    "int_parameters": [
+CP = "categorical_parameters"
+IP = "int_parameters"
+FP = "float_parameters"
+
+xgb: ParametersDictType = {
+    IP: [
         {"name": "xgb__n_estimators", "low": 100, "high": 1000},
         {"name": "xgb__max_depth", "low": 3, "high": 8},
     ],
-    "float_parameters": [
+    FP: [
         {"name": "xgb__learning_rate", "low": 0.01, "high": 0.2},
         {"name": "xgb__min_child_weight", "low": 1.0, "high": 10.0},
         {"name": "xgb__subsample", "low": 0.5, "high": 0.8},
         {"name": "xgb__colsample_bytree", "low": 0.5, "high": 0.8},
         {"name": "xgb__reg_lambda", "low": 1.0, "high": 10.0},
     ],
-    "categorical_parameters": [
+    CP: [
         {"name": "xgb__random_state", "choices": [1]},
+    ],
+}
+
+savgol: ParametersDictType = {
+    CP: [
         {"name": "savgol__win_length", "choices": [3, 5, 7, 9, 11, 13, 15]},
     ],
 }
 
+savgol_xgb: ParametersDictType = {
+    IP: xgb[IP],
+    FP: xgb[FP],
+    CP: xgb[CP] + savgol[CP],
+}
 
 PARAMETERS = {
     "savgol-xgb": savgol_xgb,
