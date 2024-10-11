2cdf954dd3b8b0384c40306dc5da4665dbc5da3f
janezlapajne <lapajne.janez@gmail.com>
2024-10-11 15:32:14 +0200

    fix: umap_display function in plots.py - simplify

--- source/analysis/plots.py
+++ source/analysis/plots.py
@@ -176,13 +176,12 @@ def umap_display(
     reducer = umap.UMAP(random_state=0)
     embeddings = reducer.fit_transform(X)
 
-    y_encoded = y_encoded
     if meta is None:
         meta = np.zeros_like(y_encoded)
 
     cmap = plt.get_cmap("Spectral")
-    if int(len(classes) / len(np.unique(meta))) > 2:
-        _colors = cmap(np.linspace(0, 1, int(len(classes) / len(np.unique(meta)))))
+    if len(classes) > 2:
+        _colors = cmap(np.linspace(0, 1, len(classes)))
     else:
         _colors = ["red", "blue"]
     _markers = ["o", "v", "^", "<", ">", "s", "p", "*", "h"]
@@ -196,20 +195,11 @@ def umap_display(
         for class_idx in np.unique(y_data_encoded_masked):
             embedding_2masked = embedding_masked[y_data_encoded_masked == class_idx]
 
-            classes_selected = np.array(classes)[np.unique(y_data_encoded_masked)]
-            classes_selected_sorted = np.sort(classes_selected)
-            classes_idx_map = {
-                idx: np.where(classes_selected_sorted == item)[0][0]
-                for idx, item in enumerate(classes_selected)
-            }
-
             ax.scatter(
                 embedding_2masked[:, 0],
                 embedding_2masked[:, 1],
                 s=50,
-                color=_colors[
-                    classes_idx_map[class_idx % len(np.unique(y_data_encoded_masked))]
-                ],
+                color=_colors[class_idx],
                 alpha=0.6,
                 marker=_markers[group_idx],
                 label=classes[class_idx] + f" s{group_idx}",
